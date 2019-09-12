74df199ba717681760a67a6d8e3aff343efe8c3e
Pokoruk <pokorukvlada@gmail.com>
2019-09-12 15:13:42 +0300

    upload linear

---
+++ lianer/src/lesson5/Main.java
@@ -0,0 +1,12 @@
+package lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        //comment
+        System.out.println("hello, world");
+        String name = "Vlada";
+        int carPrice= 5;
+        System.out.println(name);
+        System.out.println(carPrice);
+    }
+}
---
+++ lianer/src/lesson7/Main.java
@@ -0,0 +1,15 @@
+package lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 75;
+        int y = 0x4b;
+        int z = 0113;
+        int a = 0b1001011;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+
+    }
+}
